36eeab2344e9edffc66b9f523ad05b6b43225b3b
DecrypteD <batolata1337@gmail.com>
2025-06-24 07:44:33 +0800

    Update SpellInfo.cpp

--- src/server/game/Spells/SpellInfo.cpp
+++ src/server/game/Spells/SpellInfo.cpp
@@ -1910,6 +1910,7 @@ SpellCastResult SpellInfo::CheckTarget(Unit const* caster, WorldObject const* ta
     else return SPELL_CAST_OK;
 
     // corpseOwner and unit specific target checks
+
     if (!IsAllowingDeadTarget() && !unitTarget->IsAlive())
         return SPELL_FAILED_TARGETS_DEAD;
 
